2d6005e09051b34ed5d4b3fe54d48a2bbd70d553
Jonnycp <jonathan-caputo@hotmail.com>
2022-02-25 07:24:21 +0100

    Iniziata creazione modale cookie

---
+++ Wordle/assets/js/modals.js
@@ -0,0 +1,30 @@
+const generateCookieModal = (text) => {
+    let cookieContainer = document.createElement("div");
+    cookieContainer.classList.add("cookieContainer")
+
+    //MODAL HEADER
+    let modalHeader = document.createElement("header");
+    let textHeader = document.createElement("h3");
+    textHeader.appendChild(document.createTextNode("Questo sito utilizza i cookie"));
+    modalHeader.appendChild(textHeader);
+
+    let closeBtn = document.createElement("button");
+    closeBtn.classList.add("closeBtn");
+    modalHeader.appendChild(closeBtn);
+
+    cookieContainer.appendChild(modalHeader);
+
+    //MODAL TEXT
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(text));
+    cookieContainer.appendChild(p);
+
+    //BUTTON
+    let btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("ACCETTA"));
+    cookieContainer.appendChild(btn);
+
+    return cookieContainer;
+}
+
+document.body.appendChild(generateCookieModal("Questo sito utilizza i cookie e simili per riconoscere i visitatori e ricordare le loro preferenze. Per capire di più riguardo questi metodi, incluso come disabilitarli, cerca su google e informati. Questo è un banner obbligatorio dalle legge europee, ma nessuno lo leggerà tutto.\n Cliccando 'Accetta' acconsenti a trattamento dei tuoi dati con i metodi descritti sopra. Stai praticamente acconsentendo a qualsiasi trattamento commerciale dei tuoi dati sul mercato nero. Questo banner contiene informazioni false, però se l'hai letto tutto complimenti."));
