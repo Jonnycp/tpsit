7421ca7768fb1b5b339e8010d36ef18ff1fff3a5
Jonnycp <52085931+Jonnycp@users.noreply.github.com>
2022-03-16 13:41:57 +0100

    Iniziata animazione riga

--- Wordle/assets/js/animate.js
+++ Wordle/assets/js/animate.js
@@ -4,3 +4,12 @@ export const animate = (tile, animation, ms) => {
         tile.dataset.animation = "idle";
     }, ms)
 }
+
+export const animateRow = (row) => {
+    [...row.children].forEach(tile => {
+        animate(tile, "flip-in")
+        setInterval(() => {
+            animate(tile, "flip-out")
+        }, ms)
+    })
+}
