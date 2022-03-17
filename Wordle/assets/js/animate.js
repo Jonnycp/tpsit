0e44b595a34b22e81e72443f3455d76a1a4af888
Jonnycp <jonathan-caputo@hotmail.com>
2022-03-17 08:37:22 +0100

    Creato animateRow e animateSequence
    AnimateRow accetta la riga e un array lungo quanti sono i tile della riga per indicare il nuovo stato
    
    AnimafeSequence accetta un tile, un array di animazioni, il tempo in ms uguale per tutte le animazioni, l'array degli stati

--- Wordle/assets/js/animate.js
+++ Wordle/assets/js/animate.js
@@ -1,15 +1,30 @@
 export const animate = (tile, animation, ms) => {
     tile.setAttribute("data-animation", animation)
-    setInterval(() => {
+    setTimeout(() => {
         tile.dataset.animation = "idle";
     }, ms)
 }
 
-export const animateRow = (row) => {
-    [...row.children].forEach(tile => {
-        animate(tile, "flip-in")
-        setInterval(() => {
-            animate(tile, "flip-out")
-        }, ms)
-    })
-}    }
+export const animateRow = (row, state) => {
+    if(state.length == row.children.length){
+        for(let i=0; i<row.children.length; i++){
+            let tile = row.children[i];
+            setTimeout(() => {
+                animateSequence(tile, ["flip-in", "flip-out", "idle"], 250, state[i])
+            }, i * 450)
+        }
+        [...row.children].forEach(tile => tile.removeAttribute("data-animation"))
+    }else{
+        console.error("Stato tiles non valido")
+    }
+}
+
+export const animateSequence = (tile, animations, ms, state) => {
+    tile.setAttribute("data-animation", animations[0])
+    for(let i=1; i<animations.length; i++){
+        setTimeout(() => {
+            tile.dataset.animation = animations[i];
+            tile.dataset.state = state;
+        }, i * ms)
+    }
+}
