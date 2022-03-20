73f925efc25d5ac17c5e1561fc373c095307b863
Jonnycp <jonathan-caputo@hotmail.com>
2022-03-20 21:02:35 +0100

    Creato toast di errore

--- Wordle/assets/js/modals.js
+++ Wordle/assets/js/modals.js
@@ -30,6 +30,13 @@ const generateCookieModal = (text) => {
 //document.body.appendChild(generateCookieModal("Questo sito utilizza i cookie e simili per riconoscere i visitatori e ricordare le loro preferenze. Per capire di più riguardo questi metodi, incluso come disabilitarli, cerca su google e informati. Questo è un banner obbligatorio dalle legge europee, ma nessuno lo leggerà tutto.\n Cliccando 'Accetta' acconsenti a trattamento dei tuoi dati con i metodi descritti sopra. Stai praticamente acconsentendo a qualsiasi trattamento commerciale dei tuoi dati sul mercato nero. Questo banner contiene informazioni false, però se l'hai letto tutto complimenti."));
 
 const generateErrorToast = (msg, animate, row) => {
-    console.error(msg)
+    let toast = document.createElement("div");
+    toast.appendChild(document.createTextNode(msg))
+    
     animate(row, "shake", 600)
+    let toaster = document.querySelector(".toaster")
+    toaster.appendChild(toast);
+    setTimeout(() => {
+        toaster.removeChild(toast);
+    }, 1000)
 } 
