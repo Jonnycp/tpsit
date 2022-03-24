ab1f1ef1bec7bf62e0cefbb22372eb908366395e
Jonnycp <jonathan-caputo@hotmail.com>
2022-03-24 20:15:41 +0100

    Aggiunte funzionalità al modale
    - Chiusura cliccando fuori, sulla x oppure il tasto esc
    - Condivisione
    - Modale apribile dall'header (bisogna mantenere il risultato del game)

--- Wordle/assets/js/modals.js
+++ Wordle/assets/js/modals.js
@@ -43,12 +43,16 @@ const generateErrorToast = (msg, animate, row) => {
 
 //TODO: Aggiungere possibilità di toast fisso quando a fine gioco non si indovina
 //TODO: Generare Complimento in base a dove ha indovinato (da mostrare nel toast)
-const endGameModal = () => {
+const endGameModal = (end) => {
     let overlay = document.createElement("div");
     overlay.classList.add("overlay")
 
     let modal = document.createElement("div");
     modal.classList.add("modal")
+    modal.setAttribute("data-animation", "slide-in")
+    setTimeout(() => {
+        modal.dataset.animation = "idle"
+    }, 200)
 
     let closeBtnContainer = document.createElement("div")
     closeBtnContainer.classList.add("closeBtn")
@@ -71,19 +75,19 @@ const endGameModal = () => {
     let statistics = [
         {
             label: "Giocate",
-            value: localStorage.getItem("played")
+            value: localStorage.getItem("played") || 0
         },
         {
             label: "Vittorie %",
-            value: Math.trunc((parseInt(localStorage.getItem("wins"))/parseInt(localStorage.getItem("played")))*100)
+            value: Math.trunc((parseInt(localStorage.getItem("wins") || 0)/parseInt(localStorage.getItem("played") || 0) || 0)*100)
         },
         {
             label: "Streak",
-            value: localStorage.getItem("streak")
+            value: localStorage.getItem("streak") || 0
         },
         {
             label: "Max Streak",
-            value: localStorage.getItem("max-streak")
+            value: localStorage.getItem("max-streak") || 0
         },
     ]
     statistics.forEach(stat => {
@@ -102,38 +106,42 @@ const endGameModal = () => {
     });
     content.appendChild(stats);
 
-    let footer = document.createElement("footer");
-    let countdown = document.createElement("div");
-    countdown.classList.add("countdown");
-
-    let nextWordle = document.createElement("h3");
-    nextWordle.appendChild(document.createTextNode("PROSSIMO WORDLE"));
-    countdown.appendChild(nextWordle);
-
-    let timer = document.createElement("span");
-    timer.appendChild(document.createTextNode(nextWordleDate()))
-    setInterval(() => {
-        timer.innerText = nextWordleDate()
-        }, 1000)
+    if(end){
+        let footer = document.createElement("footer");
+        let countdown = document.createElement("div");
+        countdown.classList.add("countdown");
     
-    countdown.appendChild(timer);
-    footer.appendChild(countdown);
-
-    let btnShareContainer = document.createElement("div");
-    btnShareContainer.classList.add("shareBtn");
-
-    let btnShare = document.createElement("button");
-    btnShare.appendChild(document.createTextNode("CONDIVIDI"))
-    let shareIcon = document.createElement("img");
-    shareIcon.src = "./assets/img/share.svg"
-    btnShare.appendChild(shareIcon)
-    btnShareContainer.appendChild(btnShare)
-    footer.appendChild(btnShareContainer);
+        let nextWordle = document.createElement("h3");
+        nextWordle.appendChild(document.createTextNode("PROSSIMO WORDLE"));
+        countdown.appendChild(nextWordle);
     
-    content.appendChild(footer);
-
+        let timer = document.createElement("span");
+        timer.appendChild(document.createTextNode(nextWordleDate()))
+        setInterval(() => {
+            timer.innerText = nextWordleDate()
+            }, 1000)
+        
+        countdown.appendChild(timer);
+        footer.appendChild(countdown);
+    
+        let btnShareContainer = document.createElement("div");
+        btnShareContainer.classList.add("shareBtn");
+    
+        let btnShare = document.createElement("button");
+        btnShare.appendChild(document.createTextNode("CONDIVIDI"))
+        let shareIcon = document.createElement("img");
+        shareIcon.src = "./assets/img/share.svg"
+        btnShare.appendChild(shareIcon)
+        btnShareContainer.appendChild(btnShare)
+        footer.appendChild(btnShareContainer);
+        
+        content.appendChild(footer);
+    }
+    
     overlay.appendChild(modal)
     document.body.appendChild(overlay)
+
+    handleEndModal()
 }
 
 const nextWordleDate = () => {
@@ -150,3 +158,82 @@ const nextWordleDate = () => {
     }
     return `${leftDate.hours < 10 ? "0" : ""}${leftDate.hours} : ${leftDate.mins < 10 ? "0" : ""}${leftDate.mins} : ${leftDate.secs < 10 ? "0" : ""}${leftDate.secs}`
 }
+
+const closeEndModal = () => {
+    let modal = document.querySelector(".modal")
+    modal.setAttribute("data-animation", "slide-out")
+    setTimeout(() => {
+        modal.dataset.animation = "idle"
+        document.body.removeChild(document.querySelector(".overlay"))
+    }, 200)
+}
+
+//TODO: Remove eventListener to prevent spamming
+//TODO: Prevent double modal opened by stats icon in header and by endGame
+const handleEndModal = () => {
+    let closeBtn = document.querySelector(".closeBtn img");
+    let overlay = document.querySelector(".overlay");
+    let modal = document.querySelector(".modal");
+    overlay.addEventListener("click", e => {
+        if(e.target == overlay || e.target == closeBtn && modal.dataset.animation != "slide-out"){
+            closeEndModal()
+        }
+    })
+
+    document.body.addEventListener("keydown", (e) => {
+        let modal = document.querySelector(".modal");
+        if(!e.repeat){
+            if(e.code == "Escape" && modal != null && modal.dataset.animation != "slide-out"){
+                closeEndModal()
+            }
+        }
+    })
+
+    let shareBtn = document.querySelector(".shareBtn button")
+    if(shareBtn){
+        shareBtn.addEventListener("click", () => {
+            let rows = document.querySelectorAll(".board .row[data-state=validated]")
+            let message = `Wordle ${rows.length}/6 \n\n`
+            rows.forEach((row, i) => {
+                [...row.children].forEach(tile => {
+                    switch(tile.dataset.state){
+                        case "correct":
+                            message += "🟩"
+                            break;
+                        case "present":
+                            message += "🟨"
+                            break;
+                        case "nope":
+                            message += "⬜"
+                            break;
+                    }
+                })
+                i != rows.length-1 ? message += "\n" : ""
+            })
+            message += "\n\nGioca anche tu su " + window.location.href
+            navigator.share({
+                title: 'Wordle',
+                text: message,
+            })
+            .catch((error) => console.errore('Impossibile condividere', error));
+
+            navigator.clipboard.writeText(message);
+            generateErrorToast("Copiato negli appunti")
+        })
+    }
+}
+
+const handleHeaderButton = () => {
+    let btn = document.querySelectorAll("header button");
+    btn.forEach(button => {
+        button.addEventListener("click", (e) => {
+            switch(e.currentTarget.dataset.action){
+                case "stats": 
+                    endGameModal(false)
+                    break;
+            }
+        })
+    })
+}
+
+handleHeaderButton()
