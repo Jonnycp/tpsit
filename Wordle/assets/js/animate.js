e362d8e98c787c3a81ae69a665c77b2337912e90
Jonnycp <jonathan-caputo@hotmail.com>
2022-03-24 11:13:46 +0100

    Bounce Animation on win

--- Wordle/assets/js/animate.js
+++ Wordle/assets/js/animate.js
@@ -5,7 +5,7 @@ export const animate = (tile, animation, ms) => {
     }, ms)
 }
 
-export const animateRow = (row, state, word, callback, handleKeyboard) => {
+export const animateRowState = (row, state, word, end, win, updateKeyboard, handleKeyboard) => {
     if(state.length == row.children.length){
         for(let i=0; i<row.children.length; i++){
             let tile = row.children[i];
@@ -14,12 +14,29 @@ export const animateRow = (row, state, word, callback, handleKeyboard) => {
             }, i * 250)
         }
         setTimeout(() => {
-            callback(state, word)
-            handleKeyboard()
+            updateKeyboard(state, word)
+            if(!end){
+                handleKeyboard()
+            }
+            if(win){
+                animateRowWin(row)
+            }
         }, (state.length+1)*250)
     }else{
         console.error("Stato delle tiles non valido")
     }
+}
+
+export const animateRowWin = (row, ms) => {
+    for(let i=0; i<row.children.length; i++){
+        let tile = row.children[i];
+        setTimeout(() => {
+            animateSequence(tile, ["bounce", "idle"], 1000)
+        }, i * 100)
+    }
+    setTimeout(() => {
+        console.log("FINE")
+    }, row.children.length*100)
 }
 
 export const animateSequence = (tile, animations, ms, state) => {
@@ -27,7 +44,7 @@ export const animateSequence = (tile, animations, ms, state) => {
     for(let i=1; i<animations.length; i++){
         setTimeout(() => {
             tile.dataset.animation = animations[i];
-            tile.dataset.state = state;
+            state != undefined ? tile.dataset.state = state : null;
         }, i * ms)
     }
 }
